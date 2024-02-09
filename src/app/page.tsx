525b717f8d4936ca6d30f0a97c54b7702ad65edf
MAhmedSid <mahmedsiddiqui333@gmail.com>
2024-02-09 10:29:45 +0500

    meta update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,14 +10,14 @@ import Skills from "@/components/layout/Skills";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Muhammad Ahmed Siddiqui - Headless Cloud Web App Developer and Freelancer",
+  title: "Ahmed Aqil - JavaScript Web Developer",
   description:
-    "M. Ahmed Siddiqui - Full Stack Developer and Freelancer with Next JS, Cloud, AI, Serverless and Headless Web App Experience. Delivering fast, secure, and cutting-edge website solutions that leverage the power of AI. Proficient in HTML, CSS, JavaScript, React, Next.js, and other web technologies. Offering full-stack development services for modern cloud headless sites, landing pages, e-commerce sites, blogs, and AI-integrated apps. Contact for unparalleled support and satisfaction.",
-    creator: 'M. Ahmed Siddiqui ',
-    authors: { name: 'M. Ahmed Siddiqui'},
+    "Ahmed Aqil - JavaScript Developer and Freelancer with Next JS, Cloud, AI, Serverless and Headless Web App Experience. Delivering fast, secure, and cutting edge website solutions that leverage the power of AI. Proficient in HTML, CSS, JavaScript, React, Next.js, and other web technologies. Offering fast, secure, and cost efficient development services for modern websites UI, landing pages, eCommerce sites, blogs, and AI integrated web apps. Contact for unparalleled support and satisfaction.",
+    creator: 'Ahmed Aqil ',
+    authors: { name: 'Ahmed Aqil'},
     applicationName: 'Portfolio of Mr.Ahmed Dev.',
-    keywords: ["Freelancer",'Portfolio','Full Stack Developer','Modern Website Developer','Web developer','Next js developer','Cloud Developer','Ahmed','Siddiqui','Headless Developer' ],
-    category: 'technology',
+    keywords: ['Ahmed Aqil','Ahmed','Aqil',"Website Developer",'Portfolio','JavaScript Developer','React Developer','Next js developer', ],
+    category: 'portfolio',
     themeColor: '#3d64fe',
     viewport: {
       width: 'device-width',
